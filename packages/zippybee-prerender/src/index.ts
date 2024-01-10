31a40ad0ab13bb4583f005cb089075b0dc5649e2
forget <fhs8825@gmail.com>
2024-01-10 14:45:39 +0800

    feat: 添加预渲染插件

--- packages/zippybee-prerender/src/index.ts
+++ packages/zippybee-prerender/src/index.ts
@@ -1,8 +1,6 @@
 import Prerenderer from './Prerenderer';
-import DeepMerge from './merge';
 import IRenderer, { RenderedRoute, RendererConstructor } from './IRenderer';
 import { PrerendererOptions, PrerendererFinalOptions } from './PrerendererOptions';
 
 export type { RendererConstructor, IRenderer, Prerenderer, RenderedRoute, PrerendererOptions, PrerendererFinalOptions };
 export default Prerenderer;
-export * as DeepMerge from './merge';
